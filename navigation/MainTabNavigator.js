150b8dc7f81bdb806473f64a50a66678e7b35d7f
Flubie <59017383+Flubie@users.noreply.github.com>
2020-04-01 12:27:00 +0200

    import image
    wenn man image nutzen möchte sollte man auch image importieren...

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { 
+	Platform,
+	Image 
+} from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 
